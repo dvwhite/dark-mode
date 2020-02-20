ac79e7d9a86e9c724784aa19224a5d2d016749a6
David White <dwhite0627@gmail.com>
2020-02-19 20:29:50 -0500

    Fix issue with useDarkMode defaulting to false on initial mount

--- src/components/hooks/useDarkMode.js
+++ src/components/hooks/useDarkMode.js
@@ -2,10 +2,15 @@ import React, { useEffect } from 'react';
 import useLocalStorage from './useLocalStorage';
 
 const useDarkMode = () => {
-  const [isDarkMode, setIsDarkMode] = useLocalStorage('isDarkmode');
+  const [isDarkMode, setIsDarkMode] = useLocalStorage('isDarkmode', false);
 
-  useEffect(() => { 
-    body.classList.toggle('dark-mode')
+  useEffect(() => {
+    const body = document.querySelector('body');
+    if (isDarkMode) {
+      body.classList.add('dark-mode')
+    } else {
+      body.classList.remove('dark-mode');
+    }
   }, [isDarkMode])
 
   return [isDarkMode, setIsDarkMode];
